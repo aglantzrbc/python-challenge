77a4c07c4a97c58d6d9d79b77ab27cacd2e7b610
Adam <adam.glantz1@outlook.com>
2023-04-30 11:43:50 -0400

    latest moduel3 hw push

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,16 +1,35 @@
 # import module for creating file paths across operating systems for this project
 import os
-print(os.getcwd())
 
 # import module for reading csv files
 import csv
 
-csvpath = os.path.join("python-challenge", "PyBank", "Resources", "budget_data.csv")
+# create lists to store data
+total_number = []
+profit_losses = []
+
+# get data from correct path
+pybankpath = os.path.join("python-challenge", "PyBank", "Resources", "budget_data.csv")
 
 # read csv resource file for this project
-with open(csvpath) as csvfile:
-
+with open(pybankpath) as pybankfile:
+    
     # specify delimiter and variable that holds contents of file
-    csvreader = csv.reader(csvfile)
+    pybankreader = csv.reader(pybankfile)
 
-    print(csvreader)
+    # add data as lists to variables
+    for row in pybankreader:
+        total_number.append(row[0])
+        profit_losses.append(row[1])
+
+# calculate number of months in relevant list
+num_months = int(len(total_number) - 1)
+
+# print title and dashed line with proper line spacing
+print("\nFinancial Analysis")
+print("\n------------------------------\n")
+
+# print total number of months in dataset
+print(f"Total Months: {num_months}")
+
+
